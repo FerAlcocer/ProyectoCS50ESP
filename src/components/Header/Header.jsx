5c52ff24b77bc918b3f6422a7bc7d6f7115b9ba9
Fernando Alcocer <fernandoalcocer812@hotmail.com>
2023-08-04 23:45:27 -0300

    Empezando con las media query

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,26 +8,32 @@ export const Header = () => {
         <div className="header__principal">
           <img className="header__img" src={images.logo} alt="" />
           <div className="header__title title">
-            <p className="title">HarvardX CS50x</p>
-            <p className="title--bold">
-              CS50's Introduccion a la Ciencia de la Computación
-            </p>
-          </div>
-          <div className="header__user user">
-            <p className="user__title">Ayuda</p>
-            <button className="header__button">Usuario</button>
+            <div className="header__title--info">
+              <p className="title">HarvardX CS50x</p>
+              <p className="title--bold">
+                CS50's Introduccion a la Ciencia de la Computación
+              </p>
+            </div>
+
+            <hr />
+            <div className="header__user user">
+              <p className="user__title">Ayuda</p>
+              <button className="header__button">Usuario</button>
+            </div>
           </div>
         </div>
+
         <ul className="header__nav nav">
           <li className="nav__item">Curso</li>
           <li className="nav__item">Fechas</li>
           <li className="nav__item">Discusion</li>
-          <li className="nav__item">FAQS</li>
+          <li className="nav__item">FAQs</li>
           <li className="nav__item">Progreso</li>
           <li className="nav__item">Temario</li>
-          <li className="nav__item">WhatsNew</li>
+          <li className="nav__item">Novedades</li>
         </ul>
       </header>
+      <hr />
     </>
   );
 };
