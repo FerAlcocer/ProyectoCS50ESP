67f27c3c843dc54feb74c85fc7ec327f6c9510df
Fernando Alcocer <fernandoalcocer812@hotmail.com>
2023-08-06 23:44:27 -0300

    Evaluando como hacer el componente MenuDesplegable

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,9 @@ export const Header = () => {
     <>
       <header className="header">
         <div className="header__principal">
-          <img className="header__img" src={images.logo} alt="" />
+          <div className="wrapper__img">
+            <img className="header__img" src={images.logo} alt="" />
+          </div>
           <div className="header__title title">
             <div className="header__title--info">
               <p className="title">HarvardX CS50x</p>
@@ -15,7 +17,6 @@ export const Header = () => {
               </p>
             </div>
 
-            <hr />
             <div className="header__user user">
               <p className="user__title">Ayuda</p>
               <button className="header__button">Usuario</button>
@@ -23,15 +24,17 @@ export const Header = () => {
           </div>
         </div>
 
-        <ul className="header__nav nav">
-          <li className="nav__item">Curso</li>
-          <li className="nav__item">Fechas</li>
-          <li className="nav__item">Discusion</li>
-          <li className="nav__item">FAQs</li>
-          <li className="nav__item">Progreso</li>
-          <li className="nav__item">Temario</li>
-          <li className="nav__item">Novedades</li>
-        </ul>
+        <nav>
+          <ul className="header__nav nav">
+            <li className="nav__item">Curso</li>
+            <li className="nav__item">Fechas</li>
+            <li className="nav__item">Discusion</li>
+            <li className="nav__item">FAQs</li>
+            <li className="nav__item">Progreso</li>
+            <li className="nav__item">Temario</li>
+            <li className="nav__item">Novedades</li>
+          </ul>
+        </nav>
       </header>
       <hr />
     </>
